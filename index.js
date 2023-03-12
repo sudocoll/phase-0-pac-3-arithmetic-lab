9560663b6a682c3c326630f8af4b26e9ce64194c
Collette Charles <collettecra123@gmail.com>
2023-03-12 15:29:04 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a,b){
+    return (a + b)  } 
+function subtract(a,b){
+    return (a - b)
+ }
+function multiply(a,b){
+    return (a * b)
+ }
+function divide(a,b){
+    return (a / b)
+ }
+function increment(number) {
+    return (number += 1)
+}
+function decrement(number) {
+    return (number -= 1)
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+    
+}function preserveDecimal(string) {
+    return parseFloat(string, 10)}
